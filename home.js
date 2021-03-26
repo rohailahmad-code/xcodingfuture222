c8c83d382317a8aa2220ab3ccfc48aac86d9c878
Xcoding Future <70832936+rohailahmad-code@users.noreply.github.com>
2021-03-26 11:49:03 +0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,25 @@
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i=0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode
+        console.log('Option clicked', mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if(mode == 'blue'){
+        document.getElementById('theme-style').href = 'css/style.css'
+    }
+    if(mode == 'light'){
+        document.getElementById('theme-style').href = 'css/light.css'
+    }
+    if(mode == 'green'){
+        document.getElementById('theme-style').href = 'css/green.css'
+    }
+    if(mode == 'purple'){
+        document.getElementById('theme-style').href = 'css/purple.css'
+    }
+}
